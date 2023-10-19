0a55484337fe44710b389ef7e4add54fea1b7428
Prestijhonny <wprestigiovanni@yahoo.it>
2023-10-19 09:28:32 +0200

    Update all

---
+++ hw2/C#/Part1/part1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Data;
+using System.Dynamic;
+using System.IO;
+using System.Threading.Tasks.Dataflow;
+
+class Program
+{
+    static void Main(){
+        string path = "Professional Life - Sheet1.tsv";
+        string[,] matrix = tsvToMatrix(path);
+
+        // Qualitative
+        string[] instrumentsArray = getColumn(matrix,"Play some instruments? Which ones?");
+
+        Dictionary<string, float> instrumentsDictionary = new Dictionary<string, float>();
+        foreach (string i in instrumentsArray){
+            string item = i.ToLower();
+            if (instrumentsDictionary.ContainsKey(item)){
+                instrumentsDictionary[item]++;
+            } else {
+                instrumentsDictionary[item] = 1;
+            }
+        }
+
+        // printPercentage(instrumentsDictionary, instrumentsArray.Length);
+
+
+        // Quantitative discrete
+        string[] ambitiousArray = getColumn(matrix,"Ambitious (0-5)");
+
+        Dictionary<string, float> ambitiousDictionary = new Dictionary<string, float>();
+        foreach (string i in ambitiousArray){
+            string item = i.ToLower();
+            if (ambitiousDictionary.ContainsKey(item)){
+                ambitiousDictionary[item]++;
+            } else {
+                ambitiousDictionary[item] = 1;
+            }
+        }
+        
+        // printPercentage(ambitiousDictionary, ambitiousArray.Length);
+        
+        // Quantitative continuous
+        string[] weightArray = getColumn(matrix,"weight");
+        
+        Dictionary<string, float> weightDictionary = new Dictionary<string, float>();
+        weightDictionary.Add("50-", 0);
+        weightDictionary.Add("[50;60)", 0);
+        weightDictionary.Add("[60;70)", 0);
+        weightDictionary.Add("[70;80)", 0);
+        weightDictionary.Add("80+", 0);
+
+        int weightUnits = 0;
+
+        foreach (string i in weightArray){
+            string item = i.ToLower();
+            if(item.Length != 0){
+                weightUnits++;
+                int w = int.Parse(item);
+                if (w < 50) {
+                    weightDictionary["50-"] += 1;
+                } else if (w >= 50 && w < 60) {
+                    weightDictionary["[50;60)"] += 1;
+                } else if (w >= 60 && w < 70) {
+                    weightDictionary["[60;70)"] += 1;
+                } else if (w >= 70 && w < 80) {
+                    weightDictionary["[70;80)"] += 1;
+                } else if (w >= 80) {
+                    weightDictionary["80+"] += 1;
+                }
+            }
+        }
+
+
+    }
+
+    public static string[,] tsvToMatrix(string path){
+        string[] lines = File.ReadAllLines(path);
+
+        int numRows = lines.Length;
+        int numCols = lines[0].Split('\t').Length;
+
+        string[,] matrix = new string[numRows, numCols];
+        for(int i = 0; i < numRows; i++){
+            string[] splits = lines[i].Split('\t');
+            
+            for(int j = 0; j < numCols; j++){
+                matrix[i,j] = splits[j];
+            }
+        }
+        return matrix;
+    }
+
+    public static string[] getColumn(string[,] matrix, string columname){ 
+        string[] column = new string[matrix.GetLength(0)-1];
+        
+        for (int j = 0; j < matrix.GetLength(1); j++){
+           if (matrix[0, j] == columname){
+            for (int i = 1; i < matrix.GetLength(0); i++){
+                column[i-1] = matrix[i, j];
+            }
+            break; 
+            }
+        }
+        return column;
+    }
+
+    public static int getIndex(string[,] matrix, string variable){
+        int index = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++) {
+
+            if (matrix[0, j] == variable) {
+                index = j;
+                break;
+            }
+        }
+        return index;
+    }
+    public static void printColumn(string[] column){
+        foreach(var values in column){
+            Console.WriteLine(values);
+        }
+    }
+
+    public static void printMatrix(string[,] matrix){
+        for (int row = 0; row < matrix.GetLength(0); row++){
+            for (int col = 0; col < matrix.GetLength(1); col++){
+                Console.Write(matrix[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void printAbsolute(Dictionary<string, float> dictionary){
+        foreach (var kvp in dictionary){
+            Console.WriteLine($"Key: {kvp.Key} ---> Absolute frequency: {kvp.Value}");
+        }
+    }
+
+    public static void printRelative(Dictionary<string, float> dictionary, int len){
+        foreach (var kvp in dictionary){
+             Console.WriteLine($"Key: {kvp.Key} ---> Relative frequency: {kvp.Value/len}");
+        }
+    }
+
+    public static void printPercentage(Dictionary<string, float> dictionary, int len){
+        foreach (var kvp in dictionary){
+             Console.WriteLine($"Key: {kvp.Key} ---> Percentage frequency: {(kvp.Value/len)*100}%");
+        } 
+    }
+}
--- hw2/Javascript/Part1/part1.js
+++ hw2/Javascript/Part1/part1.js
@@ -1,5 +1,25 @@
 "use strict";
 
+function readData() {
+    const fs = require("fs");
+
+    const tsvData = fs.readFileSync("Professional Life.tsv", "utf8");
+    const rows = tsvData.split("\n");
+    const firstRow = rows[0].split("\t");
+    const outputData = [];
+
+    for (let i = 1; i < rows.length; i++) {
+        const line = rows[i].split("\t");
+        if (line.length === firstRow.length) {
+            const jsonRow = {};
+            for (let j = 0; j < firstRow.length; j++)
+                jsonRow[firstRow[j]] = line[j];
+            outputData.push(jsonRow);
+        }
+    }
+    return outputData;
+}
+
 function intervalCreation(d) {
     d.sort(function (a, b) { return a - b });
     let start = d[0];
@@ -16,54 +36,117 @@ function intervalCreation(d) {
 
     return intervals;
 }
-
-function createVariable(){
-    let xlsx = require("xlsx");
-    let fileName = "Professional Life.xlsx";
-    const data = xlsx.readFile(fileName);
-    const sheet = data.Sheets[data.SheetNames[0]];
-    const range = ['Q', 'S', 'Z'];
-    const MAX_ROWS = 64;
-    let res = {};
-    let out = [];
-    let j = 0;
-    range.forEach((col) => {
-        res[col] = [];
-        let frequencyAbs = {};
-        for (let i = 2; i < MAX_ROWS; i++) {
-            let cellData = sheet[`${col}${i}`] ? sheet[`${col}${i}`].v : '';
-            if (cellData != '') {
-                if (col != 'S')
-                    cellData = cellData.toString().trim();
-                res[col].push(cellData);
-            }
+// Age, weight, dream works
+function createVariable(inputData) {
+    for (let i = 0; i < inputData.length; i++) {
+        for (const key in inputData[i]) {
+            if (key != 'Age' && key != 'weight' && key != 'Dream Works')
+                delete inputData[i][key];
         }
-        res[col].forEach((value) => {
-            if (frequencyAbs[value])
-                frequencyAbs[value]++;
-            else
-                frequencyAbs[value] = 1;
-        });
-        if (col == 'S') 
-            res[col] = intervalCreation(res[col]);
-        let ind = j+1
-        out[range[j]] = {};
-        out[range[j]]["Variable "+ ind] = res[col];
-        out[range[j]]["Frequencies"] = {};
-        out[range[j]]["Frequencies"]["Absolute"] = frequencyAbs.data;
-        out[range[j]]["Frequencies"]["Relative"] = {};
-        out[range[j]]["Frequencies"]["Percentage"] = {};
-        j++;
+    }
+    const filteredArray = inputData.filter(item => {
+        return Object.values(item).every(value => value.trim() !== '' && value.trim() !== ' ');
     });
 
-    return out;
+    return filteredArray;
 }
 
-function jointDistribution(variables, k){
-    
+function frequencyAbsolute(variables) {
+    let quantContinuous = {
+        "50-": 0,
+        "[50;60)": 0,
+        "[60;70)": 0,
+        "[70;80)": 0,
+        "80+": 0,
+    }, quantDiscrete = {}, qualitative = {};
+    //Absolute frequency
+    for (let d of variables) {
+        for (const key in d) {
+            if (key == 'weight') {
+                if (d[key] < 50) {
+                    quantContinuous["50-"] += 1;
+                } else if (d[key] >= 50 && d[key] < 60) {
+                    quantContinuous["[50;60)"] += 1;
+                } else if (d[key] >= 60 && d[key] < 70) {
+                    quantContinuous["[60;70)"] += 1;
+                } else if (d[key] >= 70 && d[key] < 80) {
+                    quantContinuous["[70;80)"] += 1;
+                } else if (d[key] >= 80) {
+                    quantContinuous["80+"] += 1;
+                }
+
+            } else if (key == 'Age') {
+                if (quantDiscrete.hasOwnProperty(d[key]))
+                    quantDiscrete[d[key]]++;
+                else
+                    quantDiscrete[d[key]] = 1;
+
+            } else if (key == 'Dream Works') {
+                if (qualitative.hasOwnProperty(d[key]))
+                    qualitative[d[key]]++;
+                else
+                    qualitative[d[key]] = 1;
+            }
+        }
+    }
+    let outRes = [quantContinuous, quantDiscrete, qualitative];
+    console.log(outRes);
+    return outRes;
 }
 
-let variables = createVariable();
-//let jVar = jointDistribution(variables, k);
+function frequencyRelative(varWithAbsFreq, total) {
+    for (let d of varWithAbsFreq) {
+        for (const key in d)
+            d[key] = (d[key] / total).toFixed(4);
+    }
+    console.log(varWithAbsFreq);
+    return varWithAbsFreq;
+}
 
-console.log(variables);
+function frequencyPerc(varWithRelFreq, total) {
+    for (let d of varWithRelFreq) {
+        for (const key in d)
+            d[key] = ` ${(d[key] * 100).toFixed(2)}%`;
+    }
+    console.log(varWithRelFreq);
+    return varWithRelFreq;
+}
+
+function jointDistribution(inputData) {
+
+    const occurrences = {};
+    inputData.forEach(item => {
+        const age = item.Age;
+        const weight = item.weight;
+
+        const key = `${age}-${weight}`;
+        if (occurrences[key]) {
+            occurrences[key]++;
+        } else {
+            occurrences[key] = 1;
+        }
+    });
+    const uniqueAges = Array.from(new Set(inputData.map(item => item.Age)));
+    const uniqueWeights = Array.from(new Set(inputData.map(item => item.weight)));
+    const table = [['Age', ...uniqueWeights]];
+
+    uniqueAges.forEach(age => {
+        const rowData = [age];
+        uniqueWeights.forEach(weight => {
+            const key = `${age}-${weight}`;
+            rowData.push(occurrences[key] || 0);
+        });
+        table.push(rowData);
+    });
+
+    console.table(table);
+}
+
+
+
+let inputData = readData();
+let variables = createVariable(inputData);
+let varWithAbsFreq = frequencyAbsolute(variables);
+let varWithRelFreq = frequencyRelative(varWithAbsFreq, variables.length);
+frequencyPerc(varWithRelFreq, variables.length);
+jointDistribution(variables);
