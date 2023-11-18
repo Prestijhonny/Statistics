4f4656b06c3f19113cf1be5f8b05e82926f54916
Prestijhonny <wprestigiovanni@yahoo.it>
2023-11-18 11:36:14 +0100

    Update script.js

--- hw9/script.js
+++ hw9/script.js
@@ -3,6 +3,7 @@ const chartID = "myCanvas";
 const canvas1 = document.getElementById("canvas1");
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
+const numberOfLine = 10;
 let canvasArr = [];
 
 function destroyCanvas() {
@@ -89,118 +90,144 @@ function getRandomRGBAColor() {
 //   --------------------
 // Function to generate Arithmetic Brownian Motion data
 function generateArithmeticBrownianMotion() {
-    let numSteps = 100, mu = 0.1, sigma = 0.2, X0 = 0, dt = 0.01;
+    let numSteps = 100;
 
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    const yValues = [X0];
+    let labelGraph = "Arithmetic Brownian Motion";
+    initializeGraph(labelGraph);
 
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const newValue = (mu * dt + sigma * dW);
-        yValues.push(yValues[i] + newValue);
+    for (let j = 0; j < numberOfLine; j++) {
+
+        let mu = Math.random() * (0.3 - 0.1) + 0.1, sigma = Math.random() * (0.4 - 0.1) + 0.1, X0 = 0, dt = 0.01;
+        const yValues = [X0];
+
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const newValue = (mu * dt + sigma * dW);
+            yValues.push(yValues[i] + newValue);
+        }
+        addLine(xValues, yValues);
     }
 
-    let labelGraph = "Arithmetic Brownian Motion";
-    drawGraph(xValues, yValues, labelGraph);
 }
 
 //   --------------------
 // Function to generate Geometric Brownian Motion data
 function generateGeometricBrownianMotion() {
-    const mu = 1;
-    const sigma = 0.2;
-    const dt = 0.01;
-    const S0 = 1;
     const numSteps = 100;
-
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    const yValues = [S0];
 
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const newValue = mu * yValues[i] * dt + sigma * yValues[i] * dW;
-        yValues.push(yValues[i] + newValue);
-    }
     let labelGraph = "Geometric Brownian Motion";
-    drawGraph(xValues, yValues, labelGraph);
+    initializeGraph(labelGraph);
+    for (let j = 0; j < numberOfLine; j++) {
+        const mu = Math.random() * (1 - 0.1) + 0.1;
+        const sigma = Math.random() * (0.3 - 0.1) + 0.1;
+        const dt = 0.01;
+        const S0 = 1;
+        const yValues = [S0];
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const newValue = mu * yValues[i] * dt + sigma * yValues[i] * dW;
+            yValues.push(yValues[i] + newValue);
+        }
+        addLine(xValues, yValues);
+    }
+
 
 }
 
 //   --------------------
 // Function to generate Ornstein-Uhlenbeck
 function generateOU() {
-    let theta = 1, mu = 0, sigma = Math.sqrt(2), X0 = -10, dt = 0.05;
     let numSteps = 100;
-    let yValues = [X0];
-
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const k = theta * (mu - yValues[i]) * dt + sigma * dW;
-        yValues.push(yValues[i] + k);
-    }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    drawGraph(xValues, yValues, "Ornstein-Uhlenbeck");
+    initializeGraph("Ornstein-Uhlenbeck");
+
+    for (let j = 0; j < numberOfLine; j++) {
+        let theta = 1, mu = Math.random() * (10 - 1) + 1, sigma = Math.sqrt(2), X0 = -10, dt = 0.05;
+        let yValues = [X0];
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const k = theta * (mu - yValues[i]) * dt + sigma * dW;
+            yValues.push(yValues[i] + k);
+        }
+        addLine(xValues, yValues);
+    }
+
+
 }
 
 //   --------------------
 // Function to generate Vasicek
 function generateVasicek() {
-    let k = 0.2, theta = 0.05, sigma = 0.1, R0 = 0, dt = 0.01;
     let numSteps = 100;
-    let yValues = [R0];
-
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const rt = k * (theta - yValues[i]) * dt + sigma * Math.sqrt(dt) * dW;
-        yValues.push(yValues[i] + rt);
-    }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    drawGraph(xValues, yValues, "Vasicek");
+    initializeGraph("Vasicek");
+
+    for (let j = 0; j < numberOfLine; j++) {
+        let k = Math.random() * (0.5 - 0.2) + 0.2, theta = Math.random() * (0.6 - 0.2) + 0.2, sigma = Math.random() * (0.4 - 0.1) + 0.1, R0 = 0, dt = 0.01;
+        let yValues = [R0];
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const rt = k * (theta - yValues[i]) * dt + sigma * Math.sqrt(dt) * dW;
+            yValues.push(yValues[i] + rt);
+        }
+        addLine(xValues, yValues);
+    }
+
 }
 
 //   --------------------
 // Function to generate Hull-White
 function generateHullWhite() {
     let numSteps = 100;
-    const theta1 = 0.02, theta2 = 0.01;
-    const a = 0.1, sigma = 0.02, R0 = 0.015, dt = 0.01;
-    let yValues = [R0];
-
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const k = ((theta1 + (theta2 * i)) - (a * yValues[i])) * dt + sigma * dW;
-        yValues.push(yValues[i] + k);
-    }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    drawGraph(xValues, yValues, "Hull-White");
+
+    initializeGraph("Hull-White");
+    for (let j = 0; j < numberOfLine; j++) {
+        const theta1 = Math.random() * (0.1 - 0.02) * 0.02, theta2 = Math.random() * (0.1 - 0.01) * 0.01;
+        const a = Math.random() * (0.3 - 0.1) * 0.1, sigma = Math.random() * (0.1 - 0.02) * 0.02, R0 = 0.015, dt = 0.01;
+        let yValues = [R0];
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const k = ((theta1 + (theta2 * i)) - (a * yValues[i])) * dt + sigma * dW;
+            yValues.push(yValues[i] + k);
+        }
+        addLine(xValues, yValues);
+    }
+
 }
 
 //   --------------------
 // Function to generate Cox-Ingersoll-Ross
 function generateCoxIngersollRoss() {
     let numSteps = 100;
-    const k = 0.1, theta = 0.05, sigma = 0.02, R0 = 0.03, dt = 0.01;
-    let yValues = [R0];
-
-    for (let i = 0; i < numSteps; i++) {
-        const dW = Math.sqrt(dt) * normalDistribution();
-        const res = k*(theta-yValues[i])*dt + sigma*Math.sqrt(yValues[i])*dW;
-        yValues.push(yValues[i] + res);
-    }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    drawGraph(xValues, yValues, "Cox-Ingersoll-Ross");
-}
+    initializeGraph("Cox-Ingersoll-Ross");
 
+    for (let j = 0; j < numberOfLine; j++) {
+
+        const k = Math.random()*(0.5-0.1)+0.1, theta = Math.random()*(0.5-0.05)+0.05, sigma = Math.random()*(0.5-0.02)+0.02,R0 = 0.03, dt = 0.01;
+        let yValues = [R0];
+        for (let i = 0; i < numSteps; i++) {
+            const dW = Math.sqrt(dt) * normalDistribution();
+            const res = k * (theta - yValues[i]) * dt + sigma * Math.sqrt(yValues[i]) * dW;
+            yValues.push(yValues[i] + res);
+        }
+        addLine(xValues, yValues);
+    }
+    
+}
 //   --------------------
 // Function to generate Black-Karasinski
-function generateBlackKarasinski(){
+function generateBlackKarasinski() {
     let numSteps = 100;
-    const theta1 = 0.02, theta2 = 0.01 , a = 0.5, sigma = 0.02, R0 = 0.015, dt = 0.01;
+    const theta1 = 0.02, theta2 = 0.01, a = 0.5, sigma = 0.02, R0 = 0.015, dt = 0.01;
     let yValues = [R0];
 
     for (let i = 0; i < numSteps; i++) {
         const dW = Math.sqrt(dt) * normalDistribution();
-        const res = ((theta1+(theta2*i))-(a*Math.log(yValues[i])))*dt + sigma*Math.sqrt(yValues[i])*dW;
+        const res = ((theta1 + (theta2 * i)) - (a * Math.log(yValues[i]))) * dt + sigma * Math.sqrt(yValues[i]) * dW;
         yValues.push(yValues[i] + res);
     }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
@@ -209,17 +236,17 @@ function generateBlackKarasinski(){
 
 //   --------------------
 // Function to generate Heston
-function generateHeston(){
+function generateHeston() {
     let numSteps = 100;
     const mu = 0.05, k = 0.8, theta = 0.1, sigma = 0.2, S0 = 100, v0 = 0.2, dt = 0.01;
     let yValues = [S0];
     let v_t = [v0];
     for (let i = 0; i < numSteps; i++) {
         const dW1 = Math.sqrt(dt) * normalDistribution();
-        v_t.push((k*(theta-v_t[i])*dt+sigma*Math.sqrt(v_t[i])*dW1)+v_t[i]);
-        
+        v_t.push((k * (theta - v_t[i]) * dt + sigma * Math.sqrt(v_t[i]) * dW1) + v_t[i]);
+
         const dW2 = Math.sqrt(dt) * normalDistribution();
-        const S_t = mu*yValues[i]*dt+ Math.sqrt(v_t[i])*yValues[i]*dW2;
+        const S_t = mu * yValues[i] * dt + Math.sqrt(v_t[i]) * yValues[i] * dW2;
         yValues.push(yValues[i] + S_t);
     }
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
@@ -228,26 +255,34 @@ function generateHeston(){
 
 //   --------------------
 // Function to generate Chen
-function generateChen(){
+function generateChen() {
     let numSteps = 100;
-    let R0 = 0.05, theta0 = 0.1, sigma0 = 0.05 ;
-    let yValues = [R0]; // r_t
-    let theta_t = [theta0];
-    let sigma_t = [sigma0];
-    let a = 0.1, m = 0.1, b = 0.05, mu = 0.2, v = 0.5 , g = 0.1, dt = 0.01 , k = 0.5;
 
-
-    for (let i = 0; i < numSteps; i++) {
-        const dW1 = Math.sqrt(dt) * normalDistribution();
-        sigma_t.push((mu*(b-sigma_t[i])*dt + (m*Math.sqrt(sigma_t[i]))*dW1)+sigma_t[i]);
-        const dW2 = Math.sqrt(dt) * normalDistribution();
-        theta_t.push(theta_t[i]+(v*(g-theta_t[i])*dt + a*Math.sqrt(theta_t[i])*dW2));
-        const dW3 = Math.sqrt(dt) * normalDistribution();
-        const r_t = ((k*(theta_t[i]-yValues[i])*dt+ Math.sqrt(yValues[i])*Math.sqrt(sigma_t[i])*dW3) );
-        yValues.push(yValues[i] + r_t);
-    }
+    initializeGraph("Chen");
     const xValues = Array.from({ length: numSteps }, (_, i) => i);
-    drawGraph(xValues, yValues, "Chen");
+
+    for (let j = 0; j < numberOfLine; j++) {
+
+        let R0 = 0.05, theta0 = 0.1, sigma0 = 0.05;
+        let yValues = [R0]; // r_t
+        let theta_t = [theta0];
+        let sigma_t = [sigma0];
+        let a = Math.random() * (0.3 - 0.1) + 0.1, m = Math.random() * (0.3 - 0.1) + 0.1, b = Math.random() * (0.6 - 0.1) + 0.1, mu = Math.random() * (0.3 - 0.1) + 0.1, v = Math.random() * (0.6 - 0.1) + 0.1, g = Math.random() * (0.2 - 0.1) + 0.1, dt = 0.01, k = Math.random() * (0.6 - 0.1) + 0.1;
+
+        for (let i = 0; i < numSteps; i++) {
+            const dW1 = Math.sqrt(dt) * normalDistribution();
+            sigma_t.push((mu * (b - sigma_t[i]) * dt + (m * Math.sqrt(sigma_t[i])) * dW1) + sigma_t[i]);
+            const dW2 = Math.sqrt(dt) * normalDistribution();
+            theta_t.push(theta_t[i] + (v * (g - theta_t[i]) * dt + a * Math.sqrt(theta_t[i]) * dW2));
+            const dW3 = Math.sqrt(dt) * normalDistribution();
+            const r_t = ((k * (theta_t[i] - yValues[i]) * dt + Math.sqrt(yValues[i]) * Math.sqrt(sigma_t[i]) * dW3));
+            yValues.push(yValues[i] + r_t);
+        }
+
+        addLine(xValues, yValues);
+    }
+
+
 }
 
 //   --------------------
@@ -273,16 +308,17 @@ function normalDistribution() {
     return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 }
 
-const drawGraph = function (xValues, yValues, labelGraph) {
+function initializeGraph(labelGraph) {
     ctx.clearRect(0, 0, chartID.width, chartID.height);
+
     const myChart = new Chart(chartID, {
         type: "line",
         data: {
-            labels: xValues,
+            labels: [], // xValues
             datasets: [
                 {
                     label: labelGraph,
-                    data: yValues,
+                    data: [], // yValues
                     borderColor: "#1E1E1E",
                     fill: false,
                 },
@@ -305,3 +341,20 @@ const drawGraph = function (xValues, yValues, labelGraph) {
     });
     canvasArr.push(myChart);
 }
+
+function addLine(xValues, yValues) {
+    let chart = canvasArr[canvasArr.length - 1];
+
+    const color = getRandomRGBAColor();
+    let newDataset = {
+        fill: false,
+        lineTension: 0,
+        backgroundColor: color,
+        borderColor: color,
+        data: yValues
+    };
+    chart.data.datasets.push(newDataset);
+    chart.data.labels = xValues;
+    chart.update();
+}
+
